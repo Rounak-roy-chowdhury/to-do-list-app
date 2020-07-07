986edf9336350f236d09687a7e8dc4e2668ea5f0
Rounak-roy-chowdhury <58951218+Rounak-roy-chowdhury@users.noreply.github.com>
2020-07-07 17:38:27 +0530

    +Rounak | Code optimization done

--- server/routes/application.routes.js
+++ server/routes/application.routes.js
@@ -6,10 +6,10 @@ const applicationController = require("../controllers/application.controller");
 
 const router = express.Router();
 
-router.get('/showTask', applicationController.showTask) //done
-router.post('/addTask', applicationController.addTask) //done
-router.put('/editTask/:id', applicationController.editTask) //done
-router.post('/deleteTask/:id', applicationController.deleteTask) //done
+router.get('/showTask', applicationController.showTask) 
+router.post('/addTask', applicationController.addTask) 
+router.put('/editTask/:id', applicationController.editTask) 
+router.post('/deleteTask/:id', applicationController.deleteTask) 
 router.get('/queryTask/', applicationController.queryTask)
 
 module.exports = router;
